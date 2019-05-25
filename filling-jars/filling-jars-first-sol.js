c42de2fa909a0b406955164ece24afc1e85cd0ad
onassis osunwa <onxssis@gmail.com>
2019-05-25 13:07:52 +0100

    add better solution to filling jars

---
+++ filling-jars/filling-jars-first-sol.js
@@ -0,0 +1,12 @@
+// doesn't work with all cases, inefficient
+function solve(n, operations) {
+  const jars = Array(n).fill(0);
+  for (let i = 0; i < operations.length; i++) {
+    const [indexOne, indexTwo, value] = operations[i];
+    for (let j = indexOne - 1; j < indexTwo; j++) {
+      jars[j] = jars[j] += value;
+    }
+  }
+  const sum = jars.reduce((acc, val) => acc + val, 0);
+  return Math.floor(sum / n);
+}
